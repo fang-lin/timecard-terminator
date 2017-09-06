a81ea581b6d00daf6e5850bc73cea2090824199f
linfang <linfang@thoughtworks.com>
2017-09-06 15:57:17 +0800

    remove wait.js and empty timecard

--- scripts/timecard.js
+++ scripts/timecard.js
@@ -4,25 +4,9 @@ const fs = require('fs');
 const path = require('path');
 const util = require('./util');
 
-const logger = require('log4js').getLogger('leave');
+const logger = require('log4js').getLogger('timecard');
 logger.level = 'debug';
 
 (async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
 
-    await page.goto('https://www.google.com/');
-    await page.waitForNavigation({waitUntil: 'networkidle'});
-
-    await util.screenshot(page, logger);
-
-    const page2 = await browser.newPage();
-    await page2.goto('https://www.baidu.com/');
-    await page2.waitForNavigation({waitUntil: 'networkidle'});
-
-    await util.screenshot(page2, logger);
-
-    console.log(browser)
-
-    browser.close();
 })();
