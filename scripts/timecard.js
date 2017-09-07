09cb8bc060daf053f8b3aa3b88a9449199d148ad
linfang <linfang@thoughtworks.com>
2017-09-07 10:15:04 +0800

    Add timecard flow.

--- scripts/timecard.js
+++ scripts/timecard.js
@@ -3,10 +3,38 @@ const moment = require('moment');
 const fs = require('fs');
 const path = require('path');
 const util = require('./util');
+const oktaCookies = require('../okta-cookies.json');
+const timecardCookies = require('../timecard-cookies.json');
 
 const logger = require('log4js').getLogger('timecard');
 logger.level = 'debug';
 
 (async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const oktaPage = await browser.newPage();
 
+    await util.setCookies(oktaPage, oktaCookies);
+
+    await oktaPage.goto('https://thoughtworks.okta.com/app/UserHome');
+    await oktaPage.waitForNavigation({waitUntil: 'networkidle'});
+    await util.screenshot(oktaPage, logger);
+
+    const timecardPage = await browser.newPage();
+
+    await util.setCookies(timecardPage, timecardCookies);
+    await timecardPage.goto('https://thoughtworks.my.salesforce.com/home/home.jsp');
+    await timecardPage.waitForNavigation({waitUntil: 'networkidle'});
+
+
+    await timecardPage.waitForSelector('.wt-Timecard_Header a');
+    await (await timecardPage.$('.wt-Timecard_Header a')).click();
+    
+    await oktaPage.waitFor(10 * 1000);
+    await timecardPage.waitForSelector('#actionForm');
+    await util.screenshot(timecardPage, logger);
+
+    // await util.saveCookies(oktaPage, logger, 'okta-cookies');
+    // await util.saveCookies(timecardCookies, logger, 'timecard-cookies');
+
+    browser.close();
 })();
